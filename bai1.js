87597704b30c840bdc158e7ffb94522e2ce18379
PhieuDieu96 <PhieuDieu96@users.noreply.github.com>
2019-09-05 22:48:19 +0700

    bai1.js
    bai tesst

---
+++ bai1.js
@@ -0,0 +1,10 @@
+let num =[ 1, 3, 7, 5, 12, 18 ]
+let a, k = 0
+for ( let i = 0; i <= num.length; i++ ){
+if ( num[a] >= num[i] )
+  k = num[a]
+else if ( a == num.length-1)
+  k = num[a]
+    else ( a = i )
+  }
+  console.log(k)
