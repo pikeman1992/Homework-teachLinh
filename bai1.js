ad48c6304fc77864a1ce6fe2132f1cecb2a3deff
PhieuDieu96 <PhieuDieu96@users.noreply.github.com>
2019-09-05 23:33:47 +0700

    Update bai1.js

--- bai1.js
+++ bai1.js
@@ -1,11 +1,12 @@
 // tim so lon nhat trong chuoi so
 let num =[ 1, 3, 7, 5, 12, 18 ]
 let a, k = 0
-for ( let i = 0; i <= num.length; i++ ){
-if ( num[a] >= num[i] )
-  k = num[a]
-else if ( a == num.length-1)
-  k = num[a]
-    else ( a = i )
-  }
-  console.log(k)
+  for ( let i = 0; i <= num.length; i++ ){
+    if ( num[a] >= num[i] ){
+        k = num[a]
+    }else if ( a == num.length-1){
+        k = num[a]
+          }
+        else ( a = i )
+  } 
+console.log(k)
